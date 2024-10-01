7542c1700e69d2cdd285a17137c95040e48bdeb3
Aadarsh Adhikari <aadarshadhikar111@gmail.com>
2024-10-01 22:14:42 +0545

    basic

--- server/server.js
+++ server/server.js
@@ -7,12 +7,24 @@ dotenv.config();
 import authRouter from "./routes/auth.js";
 import categoryRoutes from "./routes/categoryRoute.js"
 import productRoute from "./routes/productRoute.js"
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 const port = process.env.PORT || 3000;
 
 // Connect to the database
 connectDB();
-
 const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+// Handle any requests that don't match an API route
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
 
 // Middleware
 app.use(cors());
